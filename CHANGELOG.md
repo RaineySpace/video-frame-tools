79aec161bba1299f25508fcbfb0a3e6900599c2a
XueRainey <xuebing_orz@qq.com>
2026-01-17 16:11:43 +0800

    chore: 修复版本0.0.3的导出配置

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # video-frame-tools
 
+## 0.0.3
+
+### Patch Changes
+
+- 修复导出文件
+
 ## 0.0.2
 
 ### Patch Changes
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: ["esm", "cjs"],
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
   dts: true,
-  sourcemap: true,
   clean: true,
-  target: "es2019",
-  splitting: false,
+  external: ['react', 'antd'],
   treeshake: true,
-  platform: "browser",
-  outDir: "dist",
-  external: []
+  sourcemap: true,
+  minify: false,
+  splitting: false,
 });
